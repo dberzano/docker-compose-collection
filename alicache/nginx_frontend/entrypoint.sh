dd3ae439ffb643447dc158e451a888473fb338e2
Dario Berzano <dario.berzano@cern.ch>
2018-11-28 15:17:49 +0100

    alicache: disable nginx caching
    It is useless with Varnish, and dangerous when we run out of space

--- alicache/nginx_frontend/entrypoint.sh
+++ alicache/nginx_frontend/entrypoint.sh
@@ -7,4 +7,10 @@ env \
 echo "=== nginx conf ==="
 cat /etc/nginx/conf.d/my.conf
 echo "=== end of nginx conf ==="
+while true; do
+  echo "Will ask nginx to reload in 8 hours"
+  sleep 28800
+  echo "Asking nginx to reload"
+  nginx -s reload
+done &
 exec nginx -g 'daemon off;'
